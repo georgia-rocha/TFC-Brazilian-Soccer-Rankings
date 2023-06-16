ee9f7404ca3813287d3fcc05a34ffc70d591bbcc
Georgia <georgiab543@gmail.com>
2023-06-16 10:05:11 -0300

    fix: corrigindo validationUser

--- app/backend/src/middlewares/validationUser.ts
+++ app/backend/src/middlewares/validationUser.ts
@@ -1,6 +1,6 @@
 // import * as jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
-// import mapStatusHTTP from '../utils/mapStatusHTTP';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
 
 export default function validateUser(req: Request, res: Response, next: NextFunction) {
   const { email, password } = req.body;
@@ -8,11 +8,11 @@ export default function validateUser(req: Request, res: Response, next: NextFunc
   const regex = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
   if (!password || !email) {
-    return res.status(400).json({ message: 'All fields must be filled' });
+    return res.status(mapStatusHTTP('INVALID_DATA')).json({ message: 'All fields must be filled' });
   }
 
-  if (regex.test(email) === false) {
-    return res.status(401).json({ message: 'Invalid email or password' });
+  if (!regex.test(email)) {
+    return res.status(mapStatusHTTP('UNAUTHORIZED')).json({ message: 'Invalid email or password' });
   }
   next();
 }
